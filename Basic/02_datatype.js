5dc7cc893cc2e49dc4204bee893ac46fdf13b5f1
Kamdev Kumar <103776332+KamdevKumar@users.noreply.github.com>
2026-02-17 10:52:53 +0000

    datatypes

---
+++ Basic/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict";// treat all js code as newer version
+console.log(3+3)//this is not a good practive
+let name="Kamdev" //String
+let age = 18 //Number
+let isLoggedIn=false //Boolean
+console.log(age);
+
+//Number
+//bigint
+//string=>""
+//boolean=>true/false
+//null=> standalone value- it represent empty value
+
+//undefined=> it means not assigned
+//symbol=> to identify uniqueness
+
+//Object
+console.log(typeof "kamdev")
+console.log(typeof age)//number
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
