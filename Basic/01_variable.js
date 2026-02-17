dd07fc1b13ad5f700eafa3107bcbbfb75103d522
Kamdev Kumar <103776332+KamdevKumar@users.noreply.github.com>
2026-02-17 11:46:31 +0000

    declaration of varable

---
+++ Basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 15445
+let accountEmail = "kamdevkumar21@gmail.com"
+var accountPassword="12345"
+accountCity = "Jaipur"
+
+//accountId = 2// not allowed
+
+accountEmail = "hdfc@gmail.com"
+accountPassword = "23454332"
+accountCity = "Chennai"
+console.log(accountId)
+console.log("Hi")
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
